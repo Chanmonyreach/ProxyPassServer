79bb74dd36730ddff05b51c0e028f369df32569b
Chanmonyreach <131956359+Chanmonyreach@users.noreply.github.com>
2025-10-03 08:45:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const PORT = 3000;
 
 // Middleware
 app.use(cors());
-app.use(express.json({ limit: '1000mb' })); // Allow larger payloads
-app.use(express.urlencoded({ limit: '1000mb', extended: true }));
+app.use(express.json({ limit: '50mb' })); // Allow larger payloads
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // ===============================
 // Configuration
 // ===============================
@@ -62,4 +62,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Proxy server listening on http://0.0.0.0:${PORT}`);
 });
+
 
